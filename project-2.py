8dd86dc9dc7f2124740d1c165f7a50a09b260729
LukasZeizinger <143009675+LukasZeizinger@users.noreply.github.com>
2023-11-08 17:51:50 +0100

    Create project-2.py

---
+++ project-2.py
@@ -0,0 +1,17 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+author: Lukas Zeizinger
+email: zeizingerlukas@gmail.com
+discord: Lukáš Zeizinger _nantuko
+"""
+
+
+print(f"Hi there!")
+print(f"-"*50)
+print(f"I've generated a random 4 digit number for you.")
+print(f"Let's play a bulls and cows game.")
+print(f"-"*50)
+
+number = str(input(f'Enter username: '))
+print(f"-"*50)
+print(f">>> ",number)
