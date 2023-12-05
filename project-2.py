1a51cb42ef1186e700d4b66db9f4abbb0163578f
LukasZeizinger <143009675+LukasZeizinger@users.noreply.github.com>
2023-12-05 14:58:35 +0100

    Update project-2.py

--- project-2.py
+++ project-2.py
@@ -5,8 +5,8 @@ email: zeizingerlukas@gmail.com
 discord: Lukáš Zeizinger _nantuko
 """
 
-import random
-
+import random  
+import time
 
 # Intro to program
 
@@ -18,10 +18,16 @@ print(f"-"*50)
 print(f"-"*50)
 
 list_num = []
+list_input_num = []
 numberx = 0
 hit_count = 0
 sep_num = {}
+input_num = {}
+sep_num_edit = {}
 keys = range(4)
+
+
+
 
 # Create a random number btw 1000 to 9999
 ran_num = str(random.randrange(1000,9999))
@@ -29,12 +35,15 @@ print(ran_num)
 # Fill sep_num by random number  
 
 for num in ran_num:
-    list_num.append(numberx)
-
+    list_num.append(int(num))
+    
 for i in keys:
-    sep_num[i] = list_num[i]
+    sep_num[i] = str(list_num[i])
+    sep_num_edit[i] = str(list_num[i])
 print(sep_num)
 
+# get the start time
+st = time.time()
 
 #TODO def zadat input a iterovat inputem přes hodnoty a zaroveň mít počítadlo a kontrolovat počítadlo a key v dictu
 
@@ -44,17 +53,66 @@ print(sep_num)
  
 while True:
     try:
-        numberx = int(input("Enter number: "))
+        numberx = str(input("Enter number: "))
     except ValueError:
         print("Please enter a number btw. 1000-9999")
         continue
-    if numberx >= 1000 and numberx <= 9999:
+    if int(numberx) >= 1000 and int(numberx) <= 9999:
+        sep_num_edit.clear
+        sep_num_edit = sep_num.copy()
         hit_count +=1
+        list_input_num.clear()
+        bull = 0
         print(f">>>  {numberx}")
         print(f"Num of shots", hit_count)
+        for n in numberx:
+            list_input_num.append((n))
+
+        for k in keys:
+            input_num[k] = list_input_num[k]
+
+            if str(input_num[k]) == str(sep_num[k]):
+                input_num[k] = True
+                sep_num_edit[k] = False
+
+                bull +=1
+            else:
+                pass
+
+        if input_num == {0: True, 1: True, 2: True, 3: True}:
+            break
+
+        else:
+            
+            cow = 0
+            for m in keys:
+                if input_num[m] in sep_num_edit.values():
+
+                    sep_num_edit[k] = False
+                    cow +=1
+                else:
+                    pass
+
+        if bull < 1 and cow < 1:
+            print(bull," bull ",cow, "cow")
+        elif bull < 1:
+            print(bull," bull ",cow, "cows")
+        else:
+            print(bull," bulls ",cow, "cows") 
     else:
         print('The integer must be in the range 1000-9999')
         break
+
+# get the end time
+et = time.time()
+
+# get the execution time
+elapsed_time = et - st
+
+if hit_count == int(1):
+    print(f'Execution time:', round(elapsed_time, 2), 'seconds and ', hit_count, "shot")
+else:
+    print(f'Execution time:', round(elapsed_time, 2), 'seconds and ', hit_count, "shots")
 
 
 #TODO Rozhodovaní 
