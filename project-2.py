580c708ac05fbb597b5f3292952861bfdd67613c
LukasZeizinger <143009675+LukasZeizinger@users.noreply.github.com>
2023-12-06 15:16:05 +0100

    Update project-2.py

--- project-2.py
+++ project-2.py
@@ -10,12 +10,8 @@ import time
 
 # Intro to program
 
-print(f"Hi there!")
-print(f"-"*50)
-print(f"I've generated a random 4 digit number for you.")
-print(f"Let's play a bulls and cows game.")
-print(f"-"*50)
-print(f"-"*50)
+print(f"Hi there!\n","-"*48)
+print(f"I've generated a random 4 digit number for you.\n","Let's play a bulls and cows game.\n","-"*48)
 
 list_num = []
 list_input_num = []
@@ -26,12 +22,12 @@ input_num = {}
 sep_num_edit = {}
 keys = range(4)
 
-
-
-
 # Create a random number btw 1000 to 9999
-ran_num = str(random.randrange(1000,9999))
+ran_num = str(7349)
+#(random.randrange(1000,9999))
+# Show the random number
 print(ran_num)
+
 # Fill sep_num by random number  
 
 for num in ran_num:
@@ -40,12 +36,11 @@ for num in ran_num:
 for i in keys:
     sep_num[i] = str(list_num[i])
     sep_num_edit[i] = str(list_num[i])
-print(sep_num)
+
 
 # get the start time
 st = time.time()
 
-#TODO def zadat input a iterovat inputem přes hodnoty a zaroveň mít počítadlo a kontrolovat počítadlo a key v dictu
 
 
 # Input from user to software
@@ -57,17 +52,21 @@ while True:
     except ValueError:
         print("Please enter a number btw. 1000-9999")
         continue
+# Check for correct number as input
     if int(numberx) >= 1000 and int(numberx) <= 9999:
-        sep_num_edit.clear
+        sep_num_edit.clear()
         sep_num_edit = sep_num.copy()
         hit_count +=1
         list_input_num.clear()
         bull = 0
+        cow = 0
+        cow_key = ()
         print(f">>>  {numberx}")
         print(f"Num of shots", hit_count)
+# Separate 4 digit number to four 1 digit numbers
         for n in numberx:
             list_input_num.append((n))
-
+# Check for bull
         for k in keys:
             input_num[k] = list_input_num[k]
 
@@ -76,29 +75,33 @@ while True:
                 sep_num_edit[k] = False
 
                 bull +=1
+            
+# Check for cow   / nefunguje zatím         
+            elif str(input_num[k]) in sep_num_edit.values():
+#                c = str(input_num[k])
+#                co = list(sep_num_edit.keys())[int(c)]
+                cow_key = sep_num_edit.get(c)
+                sep_num_edit[cow_key] = False
+                cow +=1
             else:
-                pass
-
+                    pass
+# Check if is comleted
         if input_num == {0: True, 1: True, 2: True, 3: True}:
             break
 
         else:
-            
-            cow = 0
-            for m in keys:
-                if input_num[m] in sep_num_edit.values():
-
-                    sep_num_edit[k] = False
-                    cow +=1
-                else:
-                    pass
-
-        if bull < 1 and cow < 1:
+            pass
+# Print option for bull/s and cow/s 
+        if bull < 2 and cow < 2:
             print(bull," bull ",cow, "cow")
-        elif bull < 1:
+        elif bull < 2 and cow >= 2:
             print(bull," bull ",cow, "cows")
+        elif bull >= 2 and cow < 2:
+            print(bull," bulls ",cow, "cow")
         else:
             print(bull," bulls ",cow, "cows") 
+        print(f"-"*48)
+# Condition for break if number is in noncorrect format
     else:
         print('The integer must be in the range 1000-9999')
         break
@@ -108,22 +111,8 @@ et = time.time()
 
 # get the execution time
 elapsed_time = et - st
-
+# Print option for shot/s
 if hit_count == int(1):
     print(f'Execution time:', round(elapsed_time, 2), 'seconds and ', hit_count, "shot")
 else:
     print(f'Execution time:', round(elapsed_time, 2), 'seconds and ', hit_count, "shots")
-
-
-#TODO Rozhodovaní 
-
-#TODO porovnání bulls - jestli nějaké existuje udělej
-
-#TODO porovnání cows - Jestliže číslo není bulls pak zjisti jestli není cows
-
-#TODO jestliže je bulls = 4, tak vše ukonči
-
-#TODO počítej čas od prvního zadání hodnoty
-
-#TODO počítej kolikrát hráč hádal
-
